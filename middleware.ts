fa1ac28574fdd9665eb2205e44ae75b144241dba
mtohernandez <m4teoh3rnandez@outlook.com>
2024-06-18 17:01:48 -0500

    Fix route redirecting

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,6 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
-  "/",
 ]);
 
 export default clerkMiddleware((auth, req) => {
